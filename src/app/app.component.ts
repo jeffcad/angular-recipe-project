42aaf23eea86baf451b9b7ce98c502331328a9ea
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-12-11 19:30:19 +0900

    Navigation to recipe or shopping-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+type featureType = 'recipe' | 'shopping-list';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature: featureType = 'recipe';
   title = 'course-project';
+
+  onNavigate(feature: featureType) {
+    this.loadedFeature = feature;
+  }
 }
