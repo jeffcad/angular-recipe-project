8792472df02bf6aabbedd496b870a98f061822e2
Jeff Cadieux <jeffcadieux@hotmail.com>
2021-12-20 13:35:34 +0900

    Change event emitter to subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,4 @@ type featureType = 'recipe' | 'shopping-list';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  loadedFeature: featureType = 'recipe';
-  title = 'course-project';
-
-  onNavigate(feature: featureType) {
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent { }
