51d95defaa35a11f65abc53fdf8a41aecc9ec05e
Jeff Cadieux <jeffcadieux@hotmail.com>
2022-01-19 13:50:03 +0900

    Remove exports array from recipes.module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 
@@ -24,14 +23,6 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     CommonModule,
     ReactiveFormsModule,
     RecipesRoutingModule
-  ],
-  exports: [
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeStartComponent,
-    RecipeEditComponent
   ]
 })
 export class RecipesModule {
